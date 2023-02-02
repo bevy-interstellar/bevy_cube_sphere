55b287336129843d08b867f3c4a0254e9bd98eef
Logic <38597904+LogicFan@users.noreply.github.com>
2023-02-02 13:58:19 -0500

    add example

---
+++ examples/texture.rs
@@ -0,0 +1,75 @@
+use bevy::prelude::*;
+
+use bevy::render::render_resource::Face;
+use bevy_cube_sphere::CubeSphere;
+
+#[derive(Component)]
+struct Movable;
+
+fn main() {
+    App::new()
+        .insert_resource(Msaa { samples: 4 })
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup)
+        .add_system(object_rotate)
+        .run();
+}
+
+fn setup(
+    asset_server: Res<AssetServer>,
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    let mesh: Mesh = CubeSphere::default().into();
+    let texture_handle = asset_server.load("texture/color.png");
+
+    commands.spawn((
+        PbrBundle {
+            mesh: meshes.add(mesh),
+            material: materials.add(StandardMaterial {
+                base_color_texture: Some(texture_handle.clone()),
+                cull_mode: Some(Face::Back),
+                ..default()
+            }),
+            ..default()
+        },
+        Movable,
+    ));
+
+    commands.spawn(PointLightBundle {
+        point_light: PointLight {
+            intensity: 1500.0,
+            shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        ..default()
+    });
+
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_xyz(0.0, 0.0, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
+}
+
+fn object_rotate(
+    input: Res<Input<KeyCode>>,
+    time: Res<Time>,
+    mut query: Query<&mut Transform, With<Movable>>,
+) {
+    for mut transform in &mut query {
+        if input.pressed(KeyCode::Up) {
+            transform.rotate_x(time.delta_seconds());
+        }
+        if input.pressed(KeyCode::Down) {
+            transform.rotate_x(-time.delta_seconds());
+        }
+        if input.pressed(KeyCode::Left) {
+            transform.rotate_y(time.delta_seconds());
+        }
+        if input.pressed(KeyCode::Right) {
+            transform.rotate_y(-time.delta_seconds());
+        }
+    }
+}
