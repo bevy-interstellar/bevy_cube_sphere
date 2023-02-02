5bf1e97233764e1d0fe6683505c05d306f87c7b3
Logic <38597904+LogicFan@users.noreply.github.com>
2023-02-02 02:54:15 -0500

    add tangent, fix uv

--- src/lib.rs
+++ src/lib.rs
@@ -17,6 +17,18 @@ impl Default for CubeSphere {
     }
 }
 
+#[derive(Debug, Clone, Copy)]
+enum CubeFace {
+    Front,
+    Back,
+    Left,
+    Right,
+    Top,
+    Bottom,
+}
+
+const CUBE_FACE_COUNT: usize = 6;
+
 impl From<CubeSphere> for Mesh {
     fn from(sphere: CubeSphere) -> Self {
         if sphere.resolution < 2 || sphere.resolution > 103 {
@@ -26,18 +38,12 @@ impl From<CubeSphere> for Mesh {
             );
         }
 
-        const FACE_CNT: usize = 6;
-        const UV_1: f32 = 1. / 3.;
-        const UV_2: f32 = 2. / 3.;
-        const UV_3: f32 = 1.;
+        let total_point_count = CUBE_FACE_COUNT * sphere.resolution.pow(2);
+        let mut points: Vec<[f32; 3]> = Vec::with_capacity(total_point_count);
+        let mut normals: Vec<[f32; 3]> = Vec::with_capacity(total_point_count);
+        let mut tangents: Vec<[f32; 3]> = Vec::with_capacity(total_point_count);
+        let mut uvs: Vec<[f32; 2]> = Vec::with_capacity(total_point_count);
 
-        let mut points: Vec<[f32; 3]> = Vec::with_capacity(FACE_CNT * sphere.resolution.pow(2));
-        let mut normals: Vec<[f32; 3]> = Vec::with_capacity(FACE_CNT * sphere.resolution.pow(2));
-        let mut uvs: Vec<[f32; 2]> = Vec::with_capacity(FACE_CNT * sphere.resolution.pow(2));
-        let mut indices: Vec<u32> =
-            Vec::with_capacity(FACE_CNT * 2 * (sphere.resolution - 1).pow(2));
-
-        let r = sphere.radius;
         let n = sphere.resolution as u32;
         let sep = 2_f32 / (n as f32 - 1.);
 
@@ -47,26 +53,16 @@ impl From<CubeSphere> for Mesh {
             for iy in 0..n {
                 let y = (iy as f32) * sep - 1.;
 
-                let raw = [x, y, 1.];
-                let normal = raw_to_normal(raw);
-
-                points.push(normal.map(|e| e * r));
-                normals.push(normal);
-                uvs.push([uv_scale(x), UV_3 - uv_scale(y)]);
-            }
-        }
-        // Right
-        for iz in 0..n {
-            let z = 1. - (iz as f32) * sep;
-            for iy in 0..n {
-                let y = (iy as f32) * sep - 1.;
-
-                let raw = [1., y, z];
-                let normal = raw_to_normal(raw);
-
-                points.push(normal.map(|e| e * r));
-                normals.push(normal);
-                uvs.push([UV_1 + uv_scale(z), UV_2 + uv_scale(y)]);
+                let unit_cube_point = [x, y, 1.];
+                insert_attributes(
+                    &unit_cube_point,
+                    sphere.radius,
+                    CubeFace::Front,
+                    &mut points,
+                    &mut normals,
+                    &mut tangents,
+                    &mut uvs,
+                );
             }
         }
         // Back
@@ -75,12 +71,16 @@ impl From<CubeSphere> for Mesh {
             for iy in 0..n {
                 let y = (iy as f32) * sep - 1.;
 
-                let raw = [x, y, -1.];
-                let normal = raw_to_normal(raw);
-
-                points.push(normal.map(|e| e * r));
-                normals.push(normal);
-                uvs.push([uv_scale(x), uv_scale(y)]);
+                let unit_cube_point = [x, y, -1.];
+                insert_attributes(
+                    &unit_cube_point,
+                    sphere.radius,
+                    CubeFace::Back,
+                    &mut points,
+                    &mut normals,
+                    &mut tangents,
+                    &mut uvs,
+                );
             }
         }
         // Left
@@ -89,12 +89,34 @@ impl From<CubeSphere> for Mesh {
             for iy in 0..n {
                 let y = (iy as f32) * sep - 1.;
 
-                let raw = [-1., y, z];
-                let normal = raw_to_normal(raw);
+                let unit_cube_point = [-1., y, z];
+                insert_attributes(
+                    &unit_cube_point,
+                    sphere.radius,
+                    CubeFace::Left,
+                    &mut points,
+                    &mut normals,
+                    &mut tangents,
+                    &mut uvs,
+                );
+            }
+        }
+        // Right
+        for iz in 0..n {
+            let z = 1. - (iz as f32) * sep;
+            for iy in 0..n {
+                let y = (iy as f32) * sep - 1.;
 
-                points.push(normal.map(|e| e * r));
-                normals.push(normal);
-                uvs.push([UV_1 + uv_scale(z), UV_1 - uv_scale(y)]);
+                let unit_cube_point = [1., y, z];
+                insert_attributes(
+                    &unit_cube_point,
+                    sphere.radius,
+                    CubeFace::Right,
+                    &mut points,
+                    &mut normals,
+                    &mut tangents,
+                    &mut uvs,
+                );
             }
         }
         // Top
@@ -103,12 +125,16 @@ impl From<CubeSphere> for Mesh {
             for iz in 0..n {
                 let z = 1. - (iz as f32) * sep;
 
-                let raw = [x, 1., z];
-                let normal = raw_to_normal(raw);
-
-                points.push(normal.map(|e| e * r));
-                normals.push(normal);
-                uvs.push([uv_scale(x), UV_1 + uv_scale(z)]);
+                let unit_cube_point = [x, 1., z];
+                insert_attributes(
+                    &unit_cube_point,
+                    sphere.radius,
+                    CubeFace::Top,
+                    &mut points,
+                    &mut normals,
+                    &mut tangents,
+                    &mut uvs,
+                );
             }
         }
         // Bottom
@@ -117,31 +143,23 @@ impl From<CubeSphere> for Mesh {
             for iz in 0..n {
                 let z = (iz as f32) * sep - 1.;
 
-                let raw = [x, -1., z];
-                let normal = raw_to_normal(raw);
-
-                points.push(normal.map(|e| e * r));
-                normals.push(normal);
-                uvs.push([UV_1 + uv_scale(z), UV_1 + uv_scale(x)]);
+                let unit_cube_point = [x, -1., z];
+                insert_attributes(
+                    &unit_cube_point,
+                    sphere.radius,
+                    CubeFace::Bottom,
+                    &mut points,
+                    &mut normals,
+                    &mut tangents,
+                    &mut uvs,
+                );
             }
         }
 
-        for f in 0..FACE_CNT as u32 {
-            for x in 0..n - 1 {
-                for y in 0..n - 1 {
-                    let i = f * n.pow(2) + y * n + x;
-
-                    // counter-clockwise = front
-                    indices.push(i);
-                    indices.push(i + n);
-                    indices.push(i + 1);
-
-                    indices.push(i + 1);
-                    indices.push(i + n);
-                    indices.push(i + n + 1);
-                }
-            }
-        }
+        // construct faces
+        let total_index_count = CUBE_FACE_COUNT * 2 * (sphere.resolution - 1).pow(2);
+        let mut indices: Vec<u32> = Vec::with_capacity(total_index_count);
+        insert_indices(sphere.resolution as u32, &mut indices);
 
         let mut mesh = Mesh::new(PrimitiveTopology::TriangleList);
         mesh.set_indices(Some(Indices::U32(indices)));
@@ -153,16 +171,16 @@ impl From<CubeSphere> for Mesh {
     }
 }
 
-// convert cube vertex [-1, 1] to normal
+// convert cube vertex [-1, 1] to sphere vertex [-1, 1]
 // from https://catlikecoding.com/unity/tutorials/cube-sphere/
-fn raw_to_normal(i: [f32; 3]) -> [f32; 3] {
-    let x = i[0];
-    let y = i[1];
-    let z = i[2];
+fn unit_cube_point_to_unit_sphere_point(pt: &[f32; 3]) -> [f32; 3] {
+    let x = pt[0];
+    let y = pt[1];
+    let z = pt[2];
 
-    let x2 = x.powf(2.);
-    let y2 = y.powf(2.);
-    let z2 = z.powf(2.);
+    let x2 = x.powi(2);
+    let y2 = y.powi(2);
+    let z2 = z.powi(2);
 
     let nx = x * (1. - y2 / 2. - z2 / 2. + y2 * z2 / 3.).sqrt();
     let ny = y * (1. - x2 / 2. - z2 / 2. + x2 * z2 / 3.).sqrt();
@@ -171,7 +189,123 @@ fn raw_to_normal(i: [f32; 3]) -> [f32; 3] {
     return [nx, ny, nz];
 }
 
-// convert from [-1, 1] to [0, 1/3]
-fn uv_scale(i: f32) -> f32 {
-    (i + 1.) / 6.
+fn unit_sphere_point_to_sphere_point(pt: &[f32; 3], r: f32) -> [f32; 3] {
+    return pt.map(|e| e * r);
+}
+
+fn unit_sphere_point_to_normal(pt: &[f32; 3]) -> [f32; 3] {
+    return *pt;
+}
+
+fn unit_sphere_point_to_tangent(pt: &[f32; 3], f: CubeFace) -> [f32; 3] {
+    let normal = unit_sphere_point_to_normal(pt);
+    let other = match f {
+        CubeFace::Front | CubeFace::Back | CubeFace::Left | CubeFace::Right => {
+            [0_f32, 1_f32, 0_f32]
+        }
+        CubeFace::Top | CubeFace::Bottom => [0_f32, 0_f32, 1_f32],
+    };
+
+    let x = normal[0];
+    let y = normal[1];
+    let z = normal[2];
+
+    let u = other[0];
+    let v = other[1];
+    let w = other[2];
+
+    let a = w * y - v * z;
+    let b = u * z - w * x;
+    let c = v * w - u * y;
+    let norm = (a.powi(2) + b.powi(2) + c.powi(2)).sqrt();
+
+    [a / norm, b / norm, c / norm]
+}
+
+// http://hydra.nat.uni-magdeburg.de/packing/csq/csq.html
+const UV_SPHERE_RADIUS: f32 = 0.1876806;
+const UV_COORDINATE_0: [f32; 2] = [0.0000000, 0.0000000];
+const UV_COORDINATE_1: [f32; 2] = [0.6246388, 0.0000000];
+const UV_COORDINATE_2: [f32; 2] = [0.3123194, 0.2082129];
+const UV_COORDINATE_3: [f32; 2] = [0.0000000, 0.4164259];
+const UV_COORDINATE_4: [f32; 2] = [0.6246388, 0.4164259];
+const UV_COORDINATE_5: [f32; 2] = [0.3123194, 0.6246388];
+
+fn unit_sphere_point_to_uv(pt: &[f32; 3], f: CubeFace) -> [f32; 2] {
+    let x = pt[0];
+    let y = pt[1];
+    let z = pt[2];
+
+    fn scale(i: f32) -> f32 {
+        (i + 1.) * 0.5 * UV_SPHERE_RADIUS
+    }
+
+    fn flip(i: f32) -> f32 {
+        UV_SPHERE_RADIUS - i
+    }
+
+    match f {
+        CubeFace::Front => [
+            UV_COORDINATE_0[0] + scale(x),
+            UV_COORDINATE_0[1] + flip(scale(y)),
+        ],
+        CubeFace::Back => [
+            UV_COORDINATE_3[0] + flip(scale(x)),
+            UV_COORDINATE_3[1] + flip(scale(y)),
+        ],
+        CubeFace::Left => [
+            UV_COORDINATE_2[0] + scale(z),
+            UV_COORDINATE_2[1] + flip(scale(y)),
+        ],
+        CubeFace::Right => [
+            UV_COORDINATE_5[0] + flip(scale(z)),
+            UV_COORDINATE_5[1] + flip(scale(y)),
+        ],
+        CubeFace::Top => [UV_COORDINATE_1[0] + scale(x), UV_COORDINATE_1[1] + scale(z)],
+        CubeFace::Bottom => [
+            UV_COORDINATE_4[0] + scale(x),
+            UV_COORDINATE_4[1] + flip(scale(z)),
+        ],
+    }
+}
+
+fn insert_attributes(
+    unit_cube_point: &[f32; 3],
+    radius: f32,
+    face: CubeFace,
+    points: &mut Vec<[f32; 3]>,
+    normals: &mut Vec<[f32; 3]>,
+    tangents: &mut Vec<[f32; 3]>,
+    uvs: &mut Vec<[f32; 2]>,
+) {
+    let unit_sphere_point = unit_cube_point_to_unit_sphere_point(unit_cube_point);
+    points.push(unit_sphere_point_to_sphere_point(
+        &unit_sphere_point,
+        radius,
+    ));
+    normals.push(unit_sphere_point_to_normal(&unit_sphere_point));
+    tangents.push(unit_sphere_point_to_tangent(&unit_sphere_point, face));
+    uvs.push(unit_sphere_point_to_uv(&unit_sphere_point, face))
+}
+
+fn insert_indices(n: u32, indices: &mut Vec<u32>) {
+    let edge_count = n - 1;
+    let face_count = CUBE_FACE_COUNT as u32;
+
+    for f in 0..face_count as u32 {
+        for x in 0..edge_count {
+            for y in 0..edge_count {
+                let i = f * n.pow(2) + y * n + x;
+
+                // counter-clockwise = front
+                indices.push(i);
+                indices.push(i + n);
+                indices.push(i + 1);
+
+                indices.push(i + 1);
+                indices.push(i + n);
+                indices.push(i + n + 1);
+            }
+        }
+    }
 }
